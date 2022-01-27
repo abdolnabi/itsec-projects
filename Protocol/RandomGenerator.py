ae0befaa53088f0bf476a13841c0eab8a119ee53
Abdolnabi Zameni <abdolnabi@live.com>
2022-01-27 18:28:04 +0330

    Change the random number seeder from hash_id to time() value

--- Protocol/RandomGenerator.py
+++ Protocol/RandomGenerator.py
@@ -1,27 +1,9 @@
+from random import SystemRandom
+
 class RandomGenerator:
-
-    def randgen(self,lower_bound=0,upper_bound=0):
-        rand_num = self.__hash__()
-        while rand_num < int(lower_bound):
-                rand_num += rand_num >> int(str(rand_num)[:1])
-                if rand_num > upper_bound:
-                    rand_num += rand_num >> int(str(rand_num)[:1])
-        return(rand_num)
+    @classmethod
+    def randgen(cls, upper_bound, lower_bound=1):
+        return SystemRandom().randrange(lower_bound, upper_bound)
 
 
 
-
-
-# if __name__ == "__main__":
-#     print(pow(2,1023))
-#     random_number = RandomGenerator(pow(2,1023),pow(2,1024))
-#     print(random_number.randgen)
-#     random_number = RandomGenerator(pow(2,1023),pow(2,1024))
-#     print(random_number.randgen)
-#     random_number = RandomGenerator(pow(2,1023),pow(2,1024))
-#     print(random_number.randgen)
-#     random_number = RandomGenerator(pow(2,1023),pow(2,1024))
-#     print(random_number.randgen)
-#     random_number = RandomGenerator(pow(2,1023),pow(2,1024))
-#     print(random_number.randgen)
-#     print(pow(2,1024))
