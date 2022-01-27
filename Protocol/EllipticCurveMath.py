fd1799bac66572345ad061af7f7a7fe4de1491af
Abdolnabi Zameni <abdolnabi@live.com>
2022-01-27 18:29:52 +0330

    Seperation of concerns we have in elliptic curve and its math

--- Protocol/EllipticCurveMath.py
+++ Protocol/EllipticCurveMath.py
@@ -1,40 +1,79 @@
 class EllipticCurveMath:
+    def __init__(self, p,a , z, d=0 ,q=None ):
+        self.d = d
+        self.p = p
+        self.z = z
+        self.a = a
+        self.q = q
 
-    def modInverse(self,a, m):
-        for x in range(1, m):
-            if (((a % m) * (x % m)) % m == 1):
-                return x
-        return -1
+    @staticmethod
+    def modInverse(a, m):
+        a = a% m
+        if EllipticCurveMath.gcd(a, m) != 1:
+            return None
+        u1, u2, u3 = 1, 0, a
+        v1, v2, v3 = 0, 1, m
 
+        while v3 != 0:
+            q = u3 // v3
+            v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
 
-    def calc_elliptic_curve_double(self,p,s=1,z=1):
-        x1,y1 = p
-        if p ==(0,0):
-            return 0,0
-        x2 = x1
-        y2 = y1
-        if s is not None:
-            x3 = (pow(s, 2) - x1 - x2) % z
-            y3 = (s * (x1 - x3) - y1) % z
-            return x3,y3
+        return u1 % m
+    @staticmethod
+    def gcd(a, b):
+        while a != 0:
+            a, b = b % a, a
+        return b
 
-    def calc_elliptic_curve_add(self,p,q,s=1,z=1):
+    def calc_elliptic_curve_s(self,p,q):
         x1,y1 = p
         x2,y2 = q
-        if q == (0,0):
-            return p
-        if s is not None:
-            x3 = (pow(s, 2) - x1 - x2) % z
-            y3 = (s * (x1 - x3) - y1) % z
+        if (x2 == x1 and y2 == y1) or (x2 == None and y2==None):
+            s_up = ( 3 * pow(x1, 2) + self.a ) % self.z
+            s_down = (2 * y1) % self.z
+            if s_up <0 and s_down <0:
+                s_up *= -1
+                s_down *= -1
+        else:
+            s_up = y2 - y1 % self.z
+            s_down = x2 - x1 % self.z
+            if s_up <0 and s_down <0:
+                s_up *= -1
+                s_down *= -1
+        if s_down == 0:
+            self.s = None
+            return
+        s = s_up * pow(s_down,-1,self.z)
+        self.s = s % self.z
+
+    def calc_elliptic_curve_double(self,p1):
+        x1,y1 = x2,y2 =p1
+        if p1 == (0,0):
+            return (0,0)
+        self.calc_elliptic_curve_s((x1,y1), (x2,y2))
+        if self.s is not None:
+            x3 = (pow(self.s, 2) - x1 - x2) % self.z
+            y3 = (self.s * (x1 - x3) - y1) % self.z
             return x3,y3
 
-    def multiply_point_ecc(self,d=2,p=(0,0)):
-        d_bin = "{0:b}".format(d)
+    def calc_elliptic_curve_add(self,p1):
+        x1,y1 = self.p
+        x2,y2 = p1
+        if p1 == (0,0):
+            return self.p
+        self.calc_elliptic_curve_s(self.p, p1)
+        if self.s is not None:
+            x3 = (pow(self.s, 2) - x1 - x2) % self.z
+            y3 = (self.s * (x1 - x3) - y1) % self.z
+            return x3,y3
+
+    def multiply_point_ecc(self):
+        d_bin = "{0:b}".format(self.d)
         p1=(0,0)
         for d_digit in d_bin:
             p1 = self.calc_elliptic_curve_double(p1)
             if d_digit == '1':
-                p1 = self.calc_elliptic_curve_add(p,p1)
+                p1 = self.calc_elliptic_curve_add(p1)
             if p1 is None:
                 p1 =0
         return p1
--- Protocol/EllipticCurveProtocol.py
+++ Protocol/EllipticCurveProtocol.py
@@ -1,58 +1,53 @@
 from EllipticCurveMath import EllipticCurveMath
 
 class EllipticCurveProtocol:
-    def __init__(self, a, b, P, N, Gx, Gy, name,nistName=None):
-        weierstrass_approach = 4 * pow(a, 3) + 27 * pow(b, 2)
-        if weierstrass_approach:
-            self._a = a
-            self._b = b
-            print ("All variables are set successfully!")
-        else:
-            ValueError("a and b do not meet WeierStrass approach! please recheck!!")
+    def __init__(self, a=0, b=0, P=0, N=0, Gx=0, Gy=0, name=''):
+        self.a = a
+        self.b = b
+        print ("All variables are set successfully!")
         self.z = P
         self.s = 0
         self.Xp = Gx
         self.Yp = Gy
-
+        self.p =(self.Xp,self.Yp)
+        self.order = N
         self.Xq = None
         self.Yq = None
         self.x3 = None
         self.y3 = None
         self.d = None
 
-    def set_base_point_p(self, Xp=0, Yp=0):
-        self.Xp = Xp
-        self.Yp = Yp
-        self.p = (Xp, Yp)
+    def set_base_point_p(self):
+        self.p = (self.Xp, self.Yp)
+
+
 
     def set_base_point_q(self, Xq= None, Yq=None):
-        if Xq is None and Yq is None:
-            self.Xq = 0
-            self.Yq = 0
-        else:
-            self.Xq = Xq
-            self.Yq = Yq
-        self.q = (Xq, Yq)
+        self.Yq = Yq
+        self.Xq = Xq
+        self.q = (self.Xq, self.Yq)
 
-    def calc_elliptic_curve_s(self,p,q):
-        x1,y1 = p
-        x2,y2 = q
-        if x2 == x1 and y2 == y1:
-            s_up = 3 * pow(x1, 2) + self.a
-            s_down = 2 * y1
-        else:
-            s_up = y2 - y1 % self.z
-            s_down = x2 - x1 % self.z
-        if s_down == 0:
-            self.s = None
-            return
-        self.s = int(s_up * EllipticCurveMath.modInverse(s_down,self.z)) % self.z
 
-    def isoncurve(self, p):
-        """
-        verifies if a point is on the curve
-        """
-        return not p or (p[1] ** 2 == p[0] ** 3 + self.a * p[0] + self.b)
+
+    def isoncurve(self):
+        if not 0 <= self.Xp <= self.z - 1:
+            return False
+        if not 0 <= self.Yp <= self.z - 1:
+            return False
+        if (self.Yp**2 - (self.Xp**3 + self.a * self.Xp + self.b)) % self.z != 0:
+            return False
+        return True
 
 if __name__ == "__main__":
+    bb = EllipticCurveProtocol(
+        name="secp256k1",
+        a=0,
+        b=649016698881877847465653473434873449019011128250537183611,
+        P=6277101735386680763835789423207666416083908700390324961279,
+        N=0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141,
+        Gx=602046282375688656758213480587526111916698976636884684818,
+        Gy=174050332293622031404857552280219410364023488927386650641
+    )
+    bb.calc_elliptic_curve_s()
+    print(bb.s)
     print("Do nothing")
